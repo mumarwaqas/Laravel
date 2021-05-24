e0f117fe1affdb4e805bc1f7f9139396e65e1efc
Umar Waqas <umar.waqas@inovedia.com>
2021-05-24 20:29:29 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::view('/page', 'page');
+//Route::view('/page', 'page');
